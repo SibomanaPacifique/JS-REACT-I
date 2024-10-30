9ee992c6e498364824b342398d3c13e63235929e
Sibomana Pacifique <sibomanapacifique64@gmail.com>
2024-10-30 14:45:39 -0700

    foody

---
+++ foody/src/Data.jsx
@@ -0,0 +1,60 @@
+import apple from './assets/Apple.jpg'
+import orange from './assets/orange.jpg'
+import prune from './assets/Prune.jpg'
+import pineaple from './assets/Image.jpg'
+import grapes from './assets/Grapes.jpg'
+import watermelon from './assets/water melon.jpg'
+
+const foods = [
+    { id : 1,
+        Name :"APPLE",
+        Price : 500,
+        Image : apple,
+        Description :"our apple fruit comes from our fields where we cultivate them naturaly without any push of chemical products from industry or any laboratort specimen"
+    
+    },
+    
+    { id : 1,
+        Name :"APPLE",
+        Price : 500,
+        Image : apple,
+        Description :"our apple fruit comes from our fields where we cultivate them naturaly without any push of chemical products from industry or any laboratort specimen"
+    
+    },
+    { id : 2,
+        Name :"ORANGE",
+        Price : 400,
+        Image : orange,
+        Description :"our orange fruits comes from our fields where we cultivate them naturaly without any push of chemical products from industry or any laboratort specimen"
+    
+    },
+    { id : 3,
+        Name :"PRUNE",
+        Price : 350,
+        Image : prune,
+        Description :"our prune fruits comes from our fields where we cultivate them naturaly without any push of chemical products from industry or any laboratort specimen"
+    
+    },
+    { id : 4,
+        Name :"PINEAPLE",
+        Price : 400,
+        Image : pineaple,
+        Description :"our pineaple fruit comes from our fields where we cultivate them naturaly without any push of chemical products from industry or any laboratort specimen"
+    
+    },
+    { id : 5,
+        Name :"GRAPES",
+        Price : 2000,
+        Image : grapes,
+        Description :"our pineaple fruit comes from our fields where we cultivate them naturaly without any push of chemical products from industry or any laboratort specimen"
+    
+    },
+    { id : 6,
+        Name :"WATERMELON",
+        Price : 400,
+        Image : watermelon,
+        Description :"our pineaple fruit comes from our fields where we cultivate them naturaly without any push of chemical products from industry or any laboratort specimen"
+    
+    },
+]
+export default  foods
---
+++ foody/src/Foods.jsx
@@ -0,0 +1,15 @@
+ import { useState } from 'react'
+import food from './Foods.jsx'
+ 
+ const Foods = () => {
+    const [foods, setFoods] = useState([food]);
+   return (
+     <div>
+        
+       
+     </div>
+   )
+ }
+ 
+ export default Foods
+ 
